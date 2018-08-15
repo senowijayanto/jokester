ccc93ff586556578777d73dc557b6aff6d5e0979
Seno Wijayanto <senowijayanto@gmail.com>
2018-08-15 14:24:40 +0700

    Acting Upon the Store

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,12 +1,20 @@
 <template lang="html">
   <div id="app">
     Got Jokes?
-    {{$store.state}}
+    <button class="btn btn-primary" @click="initJokes">Add Ten Random Jokes</button>
+    <div class="">
+      {{$store.state}}
+    </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
+  methods: mapActions([
+    'initJokes'
+  ])
 }
 </script>
 
